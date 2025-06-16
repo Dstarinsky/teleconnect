d35bd132cc2610e0fc47228445eeab66f6b64fe1
Dstarinsky <60348105+Dstarinsky@users.noreply.github.com>
2025-06-16 13:21:31 +0300

    Fix in prod import environment

--- main.py
+++ main.py
@@ -452,8 +452,9 @@ def main():
         fallbacks=[]
     )
 
-    app.add_handler(CallbackQueryHandler(handle_buttons))
     app.add_handler(conv_handler)
+
+    app.add_handler(CallbackQueryHandler(handle_buttons, pattern='^(?!post_ad$).*'))
     app.add_handler(CommandHandler("start", start))
     app.add_error_handler(error_handler)
 
